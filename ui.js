277884c4dd3be5808b32696f310f9ee74cf885e3
Roman <romankp@gmail.com>
2021-07-23 22:44:18 -0400

    remove once option from click handler

--- ui.js
+++ ui.js
@@ -1,16 +1,11 @@
 const initButton = (id, triggeredMethod) => {
   const buttonEl = document.getElementById(id);
 
-  buttonEl.addEventListener(
-    'click',
-    () => {
-      // This notation is a bit weird but we've basically passed findPath()
-      // with arguments, wrapped in an anonymous function, so that it's invoked here.
-      triggeredMethod();
-    },
-    // Making this button only work once for now
-    { once: id === 'find' ? true : false }
-  );
+  buttonEl.addEventListener('click', () => {
+    // This notation is a bit weird but we've basically passed findPath()
+    // with arguments, wrapped in an anonymous function, so that it's invoked here.
+    triggeredMethod();
+  });
 };
 
 export { initButton };
