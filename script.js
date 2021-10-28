88aba0d75d195a8a021cc143ce0954de692635bb
Roman <romankp@gmail.com>
2021-10-27 20:33:58 -0400

    find start/target cell order

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ import { initButton } from './ui.js';
 // Describe initial field options and construct field array.
 // The override is used to test novel field arrays and can be defined here
 const latDim = 7;
-const startPos = Math.floor(Math.random() * latDim ** 2 + 1);
+let startPos = Math.floor(Math.random() * latDim ** 2 + 1);
 const endPos = latDim ** 2;
 const override = [];
 
@@ -22,19 +22,23 @@ const fieldEl = document.getElementById('field');
 // Render field visual
 renderField(fieldArray, fieldEl, latDim);
 
-// The fun stuff.
-// Again, this will be neccessary in the future, when these are dynamic or random
-const startCell = fieldArray.find(({ type }) => type === 'start');
-const targetCell = fieldArray.find(({ type }) => type === 'target');
 let found = false;
 
 // UI stuff
 initButton('randomize', () => {
+  // Reset start position
+  startPos = Math.floor(Math.random() * latDim ** 2 + 1);
+
   fieldArray = createFieldArray(latDim, startPos, endPos, override);
   renderField(fieldArray, fieldEl, latDim);
   found = false;
 });
 initButton('find', () => {
+  // We expect the start cell (and soon the target cell) to be randomized via the UI.
+  // So we find it here, right before attempting to find the path
+  const startCell = fieldArray.find(({ type }) => type === 'start');
+  const targetCell = fieldArray.find(({ type }) => type === 'target');
+
   findPath(fieldArray, startCell, targetCell, latDim, found);
   // Prevent subsequent actions on click events until obstacle randomization occurs
   found = true;
