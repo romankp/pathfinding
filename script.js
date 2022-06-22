80adbc0edb3556461f2467a65b22d44b6bece35a
Roman <romankp@gmail.com>
2022-06-21 20:40:55 -0400

    remove some comments

--- script.js
+++ script.js
@@ -8,19 +8,12 @@ import { initUI } from './modules/ui.mjs';
 
 const fieldEl = document.getElementById('field');
 
-// Describe initial field options and construct field array
 const latDim = 10;
 let startPos = returnRandomID(latDim);
 let endPos = returnNoncollidingID(latDim, startPos);
 
 // The override is used to test novel field arrays and can be defined here
 const override = [];
-
-console.log(`lateral dim of field -> ${latDim}`);
-if (override.length < 1) {
-  console.log(`start position ID -> ${startPos}`);
-  console.log(`end position ID -> ${endPos}`);
-}
 
 let fieldArray = createFieldArray(latDim, startPos, endPos, override);
 
